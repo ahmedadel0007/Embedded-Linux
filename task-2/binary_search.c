ea1d76a71dcde557b9d2487414aab305fb1a7e35
Ahmed007 <ahmedadel5abdalla@gmail.com>
2023-12-01 10:41:51 +0200

    task2 submit

---
+++ task-2/binary_search.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int binary_search(int arr[], int size, int target) {
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == target) {
+            return mid;  // Element found, return its index
+        } else if (arr[mid] < target) {
+            low = mid + 1;  // Search in the right half
+        } else {
+            high = mid - 1;  // Search in the left half
+        }
+    }
+
+    return -1;  // Element not found
+}
+
+int main() {
+    // Declare an array (assuming it's sorted)
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    // Element to search
+    int target = 6;
+
+    // Call binary search function
+    int result = binary_search(arr, size, target);
+
+    // Print the output
+    if (result != -1) {
+        printf("Element %d found at index %d\n", target, result);
+    } else {
+        printf("Element %d not found in the array\n", target);
+    }
+
+    return 0;
+}
+
